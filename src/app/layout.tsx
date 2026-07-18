31d90c6104c5acdb68f0c94afb64d9af8dd27477
uptsapeacu-dot <sigpainel774@gmail.com>
2026-07-17 21:48:41 -0300

    analitycs versel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import './globals.css'
 import { Toaster } from '@/components/ui/sonner'
 import { ThemeProvider } from '@/components/theme-provider'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from '@vercel/analytics/next'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
@@ -80,6 +81,7 @@ export default function RootLayout({
           {children}
           <Toaster theme="dark" />
           <SpeedInsights />
+          <Analytics />
         </ThemeProvider>
         <script
           dangerouslySetInnerHTML={{
