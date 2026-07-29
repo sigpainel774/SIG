0887f193d02df31793f1ff05ba1269b4f90119d7
uptsapeacu-dot <sigpainel774@gmail.com>
2026-07-29 20:12:09 -0300

    Update DocumentosTab.tsx

--- src/components/modals/modal-funcionario/components/DocumentosTab.tsx
+++ src/components/modals/modal-funcionario/components/DocumentosTab.tsx
@@ -260,7 +260,7 @@ export function DocumentosTab() {
 
         <div>
           <Label className="text-xs text-zinc-400">Visualização no Mapa (Clique ou arraste o pino para ajustar)</Label>
-          <div className="mt-1.5 h-[240px] w-full rounded-xl overflow-hidden border border-borderCustom relative z-10">
+          <div className="mt-1.5 h-[312px] w-full rounded-xl overflow-hidden border border-borderCustom relative z-10">
             <MiniMapa
               initialLat={latitude ?? undefined}
               initialLng={longitude ?? undefined}
